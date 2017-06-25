2cd554faaa0e96aa51f4074e1feb395b9b8fd0c4
Carol St.Louis <1.selva.oscura@gmail.com>
2017-06-25 16:03:03 -0700

    added view-switcher stub (will allow toggle between user and admin views)

---
+++ src/app/view-switcher/view-switcher.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'view-switcher',
+  templateUrl: './view-switcher.component.html',
+  styleUrls: ['./view-switcher.component.css']
+})
+export class ViewSwitcherComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
