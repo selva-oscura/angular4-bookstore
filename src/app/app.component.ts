554a4229e58b200d34167e6c5edbf1f2256b6ddd
Carol St.Louis <1.selva.oscura@gmail.com>
2017-06-25 16:58:46 -0700

    added test data (testText and products) to pass from app to product-list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,24 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'Bookstore';
+
+  public title: String = 'Bookstore';
+
+  public testText: String = 'test text';
+
+  public products: Array<Object> = [  
+  	{
+  		title: "product 1",
+  		author: "A.N. Author",
+  		price: 23.99,
+  		description: "blah",
+  	},{
+  		title: "product 2",
+  		author: "An Other Author",
+  		price: 12.99,
+  		description: "blah, blah",
+  	}
+  ];
 }
