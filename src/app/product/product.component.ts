32317fec3edc521e88df1e2127c2750b7f1287b1
Carol St.Louis <1.selva.oscura@gmail.com>
2017-06-28 17:46:23 -0700

    added type to declaration of Input

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class ProductComponent implements OnInit {
 
-	@Input() public product;
+	@Input() public product: Object;
 
   constructor() { }
 
